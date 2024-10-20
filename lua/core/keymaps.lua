c5656f2778e6ec68f553da80a73de225e46ab70d
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-10-20 19:44:52 +0100

    update config files

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,4 +1,4 @@
-vim.keymap.set('n', '<C-h>', '<C-w><Left>', {desc = 'Move focus to left window'})
-vim.keymap.set('n', '<C-l>', '<C-w><Right>', {desc = 'Move focus to right window'})
-vim.keymap.set('n', '<C-j>', '<C-w><Down>', {desc = 'Move focus to lower window'})
-vim.keymap.set('n', '<C-k>', '<C-w><Up>', {desc = 'Move focus to upper window'})
+vim.keymap.set("n", "<C-h>", "<C-w><Left>", { desc = "Move focus to left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><Right>", { desc = "Move focus to right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><Down>", { desc = "Move focus to lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><Up>", { desc = "Move focus to upper window" })
--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,11 +1,11 @@
 local opt = vim.opt
 local g = vim.g
 
-g.mapleader = ' '
-g.maplocalleader = ' '
+g.mapleader = " "
+g.maplocalleader = " "
 g.have_nerd_font = true
 
-opt.mouse = 'a'
+opt.mouse = "a"
 opt.number = true
 opt.relativenumber = true
 opt.termguicolors = true
@@ -14,13 +14,21 @@ opt.breakindent = true
 opt.undofile = true
 opt.ignorecase = true
 opt.smartcase = true
-opt.signcolumn = 'yes'
+opt.signcolumn = "yes"
 opt.updatetime = 250
 opt.timeoutlen = 300
 opt.splitright = true
 opt.splitbelow = true
 opt.list = true
-opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-opt.inccommand = 'split'
+opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+opt.inccommand = "split"
 opt.cursorline = true
 opt.scrolloff = 10
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
