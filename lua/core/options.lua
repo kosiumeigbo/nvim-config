e41ca61d0dcb5c305f87b051e95efad04bc84451
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-10-20 19:31:46 +0100

    add default vim options

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,5 +1,26 @@
 local opt = vim.opt
+local g = vim.g
 
+g.mapleader = ' '
+g.maplocalleader = ' '
+g.have_nerd_font = true
+
+opt.mouse = 'a'
 opt.number = true
 opt.relativenumber = true
 opt.termguicolors = true
+opt.showmode = false
+opt.breakindent = true
+opt.undofile = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.signcolumn = 'yes'
+opt.updatetime = 250
+opt.timeoutlen = 300
+opt.splitright = true
+opt.splitbelow = true
+opt.list = true
+opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+opt.inccommand = 'split'
+opt.cursorline = true
+opt.scrolloff = 10
