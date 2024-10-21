1f15abf82283948ec0c3a32dbd532214b4a30e6b
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-10-21 19:00:34 +0100

    add keymaps for tab functions for when cursor not in a neotree window

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -3,3 +3,8 @@ vim.keymap.set("n", "<C-h>", "<C-w><Left>", { desc = "Move focus to left window"
 vim.keymap.set("n", "<C-l>", "<C-w><Right>", { desc = "Move focus to right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><Down>", { desc = "Move focus to lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><Up>", { desc = "Move focus to upper window" })
+
+-- Tab functions (DOES NOT WORK IN A NEOTREE WINDOW)
+vim.keymap.set("n", "ts", "<cmd>tab<Space>split<CR>", { desc = "Open current buffer in a new tabpage" })
+vim.keymap.set("n", "tt", "<cmd>tabnew<CR>", { desc = "Open whole new empty tabpage" })
+vim.keymap.set("n", "tc", "<cmd>tabclose<CR>", { desc = "Close current tabpage" })
