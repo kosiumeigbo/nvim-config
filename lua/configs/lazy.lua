468249ae4fbd16d91041f12ee996531abf38bbc8
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-10-27 23:38:49 +0000

    add lualine plugin

--- lua/configs/lazy.lua
+++ lua/configs/lazy.lua
@@ -65,6 +65,11 @@ require("lazy").setup({
     { -- Conform formatter
       "stevearc/conform.nvim",
     },
+
+    { -- Lualine
+      "nvim-lualine/lualine.nvim",
+      dependencies = { "nvim-tree/nvim-web-devicons" },
+    },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
