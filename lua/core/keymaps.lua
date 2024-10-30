3adedbc13b320bba0b8f8d9c0d0c2d9a517a7053
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-10-30 17:08:56 +0000

    remove comment

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -4,10 +4,6 @@ vim.keymap.set("n", "<C-l>", "<C-w><Right>", { desc = "Move focus to window to t
 vim.keymap.set("n", "<C-j>", "<C-w><Down>", { desc = "Move focus to window [B]elow" })
 vim.keymap.set("n", "<C-k>", "<C-w><Up>", { desc = "Move focus to window [A]bove" })
 
--- Window creation
--- vim.keymap.set("n", "wl", "<cmd>vsplit<CR>", { desc = "Split [W]indow [R]ight" })
--- vim.keymap.set("n", "wj", "<cmd>split<CR>", { desc = "Split [W]indow [B]elow" })
-
 -- Tab functions (DOES NOT WORK IN A NEOTREE WINDOW)
 vim.keymap.set("n", "ts", "<cmd>tab<Space>split<CR>", { desc = "[S]plit current buffer in a new tabpage" })
 vim.keymap.set("n", "tt", "<cmd>tabnew<CR>", { desc = "Open whole new emp[T]y [T]abpage" })
