d863c817b8d33cfd06d8b4cf78367adf6cdf7c20
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-11-06 18:27:31 +0000

    add 'friendly snippets' plugin

--- lua/configs/lazy.lua
+++ lua/configs/lazy.lua
@@ -90,12 +90,13 @@ require("lazy").setup({
       "numToStr/Comment.nvim",
     },
 
-    { -- Auto-completion
+    { -- Auto-completion and Snippets
       "neovim/nvim-lspconfig", -- Collection of configurations for built-in LSP client
       "hrsh7th/nvim-cmp", -- Autocompletion plugin
       "hrsh7th/cmp-nvim-lsp", -- LSP source for nvim-cmp
       "saadparwaiz1/cmp_luasnip", -- Snippets source for nvim-cmp
       "L3MON4D3/LuaSnip", -- Snippets plugin
+      "rafamadriz/friendly-snippets",
     },
 
     { -- Auto-pairs
